9d12060486db168d8f5da5113f23e2b1d595dcd0
Abdul Razaque Goraya <razaqueg007@gmail.com>
2023-05-27 15:07:54 +0500

    Add files via upload

---
+++ src/Clock.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react';
+import './clock.css'
+
+const Clock = () => {
+    let time = new Date().toLocaleTimeString();
+    const [currentTime,setCurrentTime] = useState(time);
+
+    const updateTime = ()=>{
+        let time = new Date().toLocaleTimeString();
+        setCurrentTime(time);
+    }
+    setInterval(updateTime,1000);
+
+  return (
+    <div className='clock'>
+        <h1>{currentTime}</h1>
+    </div>
+  )
+}
+
+export default Clock
